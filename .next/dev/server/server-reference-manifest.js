5f89c5b8b6fd21d72260422cda7d37fa69353c5b
dhaval-patil21 <dhavalpatil2003@gmail.com>
2025-12-01 01:19:43 +0530

    first commit

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Jsb9X/IaW093+15THHKloYhcPoVa+ygNYbrhBooFzXk=\"\n}"
--- .next/dev/static/chunks/6f348_next_dist_client_components_builtin_global-error_d9fab134.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/6f348_next_dist_50715a0a._.js"
-],
-    source: "dynamic"
-});
